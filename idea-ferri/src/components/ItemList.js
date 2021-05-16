5939edf03cde2294f02f603ef7afc6e899e68f97
Carolina Ferri <mcarolina.f@hotmail.com>
2021-05-16 17:09:36 -0300

    se corrige itemListContainer

--- idea-ferri/src/components/ItemList.js
+++ idea-ferri/src/components/ItemList.js
@@ -4,21 +4,35 @@ import Item from '../components/Item'
 import Container from 'react-bootstrap/Container'
 import { Table } from 'react-bootstrap';
 
-function ItemList (itemList){
+import a71 from '../assets/a71.jpg'
+import g100 from '../assets/g100.jpg'
+import s21 from '../assets/s21.png'
+import note10 from '../assets/note10.jpg'
 
-   //const itemList = [
-   //    Item({id:1, pictureUrl: '/static/media/a71.6e1dd95c.jpg', title: 'a71', price:80000}),
-   //    Item({id:2, pictureUrl: '/static/media/a71.6e1dd95c.jpg', title: 'a99', price:98000}),
-   //    Item({id:3, pictureUrl: '/static/media/a71.6e1dd95c.jpg', title: 'a03', price:13000}),
-   //    Item({id:4, pictureUrl: '/static/media/a71.6e1dd95c.jpg', title: 's15', price:1234567890})
-   //]
+function mockItems() {
+    var promise = new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve([
+            Item({id:1, pictureUrl: a71, title: 'SAMSUNG GALAXY A71', price:"$65.000"}),
+            Item({id:2, pictureUrl: s21, title: 'SAMSUNG S21', price: "$119.000"}),
+            Item({id:3, pictureUrl: g100, title: 'MOTOROLA G100', price:"$79.000"}),
+            Item({id:4, pictureUrl: note10, title: 'XIAOMI MI NOTE 10', price:"$94.000"})
+        ]);
+      }, 2000);
+    });
+    return promise;
+ }
+
+async function ItemList (itemList){
     
+    const list = await mockItems()
+
     return (
     <Container fluid>   
         <Table responsive>
             <tbody>
                 <tr>
-                    {itemList.map(i => <td>{i}</td>)}
+                    {list.map(i => <td>{i}</td>)}
                 </tr>
             </tbody>
         </Table>
--- idea-ferri/src/containers/ItemListContainer.js
+++ idea-ferri/src/containers/ItemListContainer.js
@@ -1,34 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container'
-import Item from '../components/Item'
 import ItemList from '../components/ItemList'
-import a71 from '../assets/a71.jpg'
-import g100 from '../assets/g100.jpg'
-import s21 from '../assets/s21.png'
-import note10 from '../assets/note10.jpg'
-
-function mockItems() {
-    var promise = new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        resolve([
-            Item({id:1, pictureUrl: a71, title: 'SAMSUNG GALAXY A71', price:"$65.000"}),
-            Item({id:2, pictureUrl: s21, title: 'SAMSUNG S21', price: "$119.000"}),
-            Item({id:3, pictureUrl: g100, title: 'MOTOROLA G100', price:"$79.000"}),
-            Item({id:4, pictureUrl: note10, title: 'XIAOMI MI NOTE 10', price:"$94.000"})
-        ]);
-      }, 2000);
-    });
-    return promise;
- }
 
 const ItemListContainer = props => {
 
     const [data, setData] = useState();
    
     useEffect(async () => {
-      const result = await mockItems();
+      const result = await ItemList();
    
-      setData(ItemList(result));
+      setData(result);
     }, []);
 
     const greeting = <h1>¡Bienvenido a CellShop!</h1>
